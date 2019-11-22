502246f9feb606cf2f79fa1c4911d5449549598b
ThibDT <thibault.de.tourdonnet@gmail.com>
2019-11-22 12:27:05 +0100

    update data.js

--- data.js
+++ data.js
@@ -4,17 +4,18 @@ var posts = [
     { id: 3, title: "Sample article 3", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?", author: 2 }, 
     { id: 4, title: "Sample article 4", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.", author: 1 }, 
     { id: 5, title: "Sample article 5", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.", author: 3 }, 
-    { id: 7, title: "Sample article 7", thumbnail: 'http://lorempixel.com/400/200/people/', content: "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.", author: 2 }
+    { id: 6, title: "Sample article 6", thumbnail: 'http://lorempixel.com/400/200/people/', content: "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.", author: 2 }
 ];
 
 var comments = [
-    { id: 1, userId: 1, date: "05/11/2019", likes: 12, content: "First !", postId: 1},
+    { id: 1, userId: 1, date: "05/11/2019", likes: -4, content: "First!", postId: 1},
     { id: 2, userId: 1, date: "21/11/2019", likes: 45, content: "Plop", postId: 2},
     { id: 3, userId: 2, date: "12/10/2019", likes: 86, content: "Bla bla bla bla bla bla", postId: 3},
     { id: 4, userId: 3, date: "27/10/2019", likes: 4, content: "YOLOOOOOOOOOO", postId: 4},
     { id: 5, userId: 4, date: "26/10/2019", likes: 28, content: "This is just a comment.", postId: 5},
     { id: 6, userId: 4, date: "14/09/2019", likes: 97, content: "Hi !", postId: 6},
-    { id: 7, userId: 5, date: "01/11/2019", likes: 26, content: "Hello, world!", postId: 7},
+    { id: 7, userId: 5, date: "01/11/2019", likes: 26, content: "Hello, world!", postId: 6},
+    { id: 8, userId: 2, date: "05/11/2019", likes: 2, content: "U retard!", postId: 1},
 ];
 
 var authors = [
