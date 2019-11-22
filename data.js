e82ed02f2ddf26e5f826c0405b1c611e1686966c
ThibDT <thibault.de.tourdonnet@gmail.com>
2019-11-22 12:12:14 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+var posts = [
+    { id: 1, title: "Sample article 1", thumbnail: 'http://lorempixel.com/400/200/people/', content: "You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche,  it took us a week to climb out. Now,  I don't know exactly when we turned on each other,  but I know that seven of us survived the slide... and only five made it out. Now we took an oath,  that I'm breaking now. We said we'd say it was the snow that killed the other two,  but it wasn't. Nature is lethal but it doesn't hold a candle to man.",  author: 1  }, 
+    { id: 2, title: "Sample article 2", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.", author: 1 }, 
+    { id: 3, title: "Sample article 3", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?", author: 2 }, 
+    { id: 4, title: "Sample article 4", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.", author: 1 }, 
+    { id: 5, title: "Sample article 5", thumbnail: 'http://lorempixel.com/400/200/people/', content: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.", author: 3 }, 
+    { id: 7, title: "Sample article 7", thumbnail: 'http://lorempixel.com/400/200/people/', content: "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.", author: 2 }
+];
+
+var comments = [
+    { id: 1, userId: 1, date: "05/11/2019", likes: 12, content: "First !", postId: 1},
+    { id: 2, userId: 1, date: "21/11/2019", likes: 45, content: "Plop", postId: 2},
+    { id: 3, userId: 2, date: "12/10/2019", likes: 86, content: "Bla bla bla bla bla bla", postId: 3},
+    { id: 4, userId: 3, date: "27/10/2019", likes: 4, content: "YOLOOOOOOOOOO", postId: 4},
+    { id: 5, userId: 4, date: "26/10/2019", likes: 28, content: "This is just a comment.", postId: 5},
+    { id: 6, userId: 4, date: "14/09/2019", likes: 97, content: "Hi !", postId: 6},
+    { id: 7, userId: 5, date: "01/11/2019", likes: 26, content: "Hello, world!", postId: 7},
+];
+
+var authors = [
+    { id: 1, name: 'Samuel L. Ipsum' },
+    { id: 2, name: 'Mr. Jackson' },
+    { id: 3, name: 'Sam' }
+];
+
+var users = [
+    { id: 1, nickname: "Hubert" },
+    { id: 2, nickname: "Marcel" },
+    { id: 3, nickname: "Paul" },
+    { id: 4, nickname: "Richard" },
+    { id: 5, nickname: "Albus" }
+];
